70364d9e62321e312f937c717e6275c5a43bc3c5
liuhuaide <jcakcino@gmail.com>
2024-09-28 20:04:47 +0200

    20:04

---
+++ R/wiki_graph.R
@@ -0,0 +1,13 @@
+#' @title Wiki Graph Dataset
+#' @description
+#' This dataset represents a graph used for demonstrating Dijkstra's algorithm.
+#' It contains nodes (vertices) and edges (connections between nodes) with weights.
+#'
+#' @format A data frame with 3 columns:
+#' \describe{
+#'   \item{v1}{The starting node (numeric).}
+#'   \item{v2}{The ending node (numeric).}
+#'   \item{w}{The weight of the edge between the nodes (numeric).}
+#' }
+#' @source Data derived from the Dijkstra algorithm example on Wikipedia.
+"wiki_graph"
